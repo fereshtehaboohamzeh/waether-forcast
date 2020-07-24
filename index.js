c94f678cdf21c9e283229d14ad2b475b607df710
Fereshte <fe.aboohamzeh@gmail.com>
2020-07-24 23:32:17 +0430

    weather first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let request = require('request');
+const argv = require('yargs').argv;
+
+const city = argv.r || "Tehran"
+request.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e2c8ab4e035657a747207767b226c916`,(err, response, body) => {
+  if(err) console.log(err)
+    else{
+        let weather = JSON.parse(body);
+      let message = `It's ${Math.round(weather.main.temp-273.5)} degrees in ${weather.name}!`;
+      console.log( message);
+  }
+
+})
